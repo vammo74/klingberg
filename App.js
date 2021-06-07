1e836b5451a55d7b4ec580d5b89bbda3943cbe33
vammo74 <vammo74@gmail.com>
2021-06-07 22:44:59 +0200

    fixed app.js

--- App.js
+++ App.js
@@ -28,7 +28,6 @@ const App = () => {
         onUpdateLevel={updateLevelHandler}
         onChangeLevel={flag => changeLevelHandler(flag)}
       />
-      <StatusBar style="auto" />
     </SafeAreaView>
   );
 };
