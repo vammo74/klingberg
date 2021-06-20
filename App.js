064173d7472e8398eb6bdc813e54238a8ad9f5c0
vammo74 <vammo74@gmail.com>
2021-06-20 09:01:04 +0200

    changes

--- App.js
+++ App.js
@@ -1,136 +1,119 @@
-import React, { useState, useEffect } from 'react';
+import React, { Component } from 'react';
 import { StyleSheet, View, Text, SafeAreaView, StatusBar, Modal } from 'react-native';
-// import AsyncStorage from "@react-native-async-storage/async-storage";
+
 import MMKVStorage from "react-native-mmkv-storage";
 import Calculator from "./components/Calculator/Calculator";
 import Table from "./components/Table/Table";
 import InfoButton from "./components/UI/Buttons/InfoButton";
 import PopUp from "./components/UI/PopUp/PopUp";
 
-// const STORAGE_KEY = '@save_stats'
+
 
 MMKV = new MMKVStorage.Loader().initialize();
 
-const App = () => {
-  console.log("App")
-  const [level, setLevel] = useState(3);
-  const [popupped, setPopupped] = useState(false);
-  const [savedStats, setSavedStats] = useState(
-    '{"level":4,"product":"","products":["5 × 9","5 × 8","4 × 7","2 × 2","2 × 3","2 × 4","2 × 5","2 × 6","2 × 7","2 × 8","2 × 9","2 × 10","3 × 3","3 × 4","3 × 5","3 × 6","3 × 7","3 × 8","3 × 9","4 × 4","4 × 5","4 × 6","4 × 8","4 × 9","4 × 10","5 × 5","5 × 6","5 × 7","5 × 10","6 × 6","6 × 7","6 × 8","6 × 9","6 × 10","7 × 7","7 × 8","7 × 9","7 × 10","8 × 8","8 × 9","8 × 10","9 × 9","9 × 10","10 × 10"],"levelAttempts":true}'
-  );
+class App extends Component {
+  constructor() {
+    super();
+    this.state = {
+      level: 3,
+      popupped: false,
+      savedStats: { level: 4, products: ["5 × 9", "5 × 8", "4 × 7", "2 × 2", "2 × 3", "2 × 4", "2 × 5", "2 × 6", "2 × 7", "2 × 8", "2 × 9", "2 × 10", "3 × 3", "3 × 4", "3 × 5", "3 × 6", "3 × 7", "3 × 8", "3 × 9", "4 × 4", "4 × 5", "4 × 6", "4 × 8", "4 × 9", "4 × 10", "5 × 5", "5 × 6", "5 × 7", "5 × 10", "6 × 6", "6 × 7", "6 × 8", "6 × 9", "6 × 10", "7 × 7", "7 × 8", "7 × 9", "7 × 10", "8 × 8", "8 × 9", "8 × 10", "9 × 9", "9 × 10", "10 × 10"], levelAttempts: true },
+    }
+    this.saveStats = () => {
+      console.log(savedStats)
+      MMKV.setMap("savedStats", this.state.savedStats);
+    }
 
-  // const saveStats = async () => {
-  //   try {
-  //     await AsyncStorage.setItem(STORAGE_KEY, savedStats)
-  //     alert("Data successfully saved: " + savedStats)
-  //   } catch (e) {
-  //     alert('Failed to save the data to the storage')
-  //   }
-  // }
+    this.readStats = () => {
+      MMKV.getMap("savedStats", (error, result) => {
+        if (error) {
+          console.log(error);
+          return;
+        }
 
-  const saveStats = () => {
-    console.log(savedStats)
-    MMKV.setString("savedStats", savedStats);
-  }
-  
-  // const readStats = async () => {
-  //   try {
-  //     const stats = await AsyncStorage.getItem(STORAGE_KEY)
+        this.setState({ savedStats: result });
+        this.setState({level: result.level})
 
-  //     if (stats !== null) {
-  //       setSavedStats(stats);
-        
-  //     }
-  //   } catch (e) {
-  //     alert('Failed to fetch the data from storage')
-  //   }
-  // }
+        console.log("result")
+      });
+    }
 
-  const readStats = () => {
-    MMKV.getString("savedStats", (error, result) => {
-      if (error) {
-        console.log(error);
-        return;
-      }
-    
-      setSavedStats(result);
-      console.log("result: " + result)
-    });
+    this.clearStats = () => {
+      MMKV.clearStorage()
+
+    }
+
   }
 
-  // const clearStats = async () => {
-  //   try {
-  //     await AsyncStorage.clear()
-  //     alert('Storage successfully cleared!')
-  //   } catch (e) {
-  //     alert('Failed to clear the async storage.')
-  //   }
-  // }
-
-  const clearStats = () => {
-    
+  componentDidMount() {
+    readStats()
   }
 
-  // useEffect(() => {
-  //   console.log("read");
-  //   readStats();
-    
-  // }, [])
-
-  const passStatsHandler = (passedStats) => {
-    setSavedStats(passedStats);
+  componentWillUnmount() {
     saveStats()
   }
+  render() {
 
-  const changeLevelHandler = (flag) => {
-    if (flag === "up" && level <= 10) {
-      setLevel((prevLevel) => prevLevel + 1);
+    
+    const passStatsHandler = (passedStats) => {
+      let stats = {level: this.state.level, ...passedStats}
+      this.setState({savedStats: stats});
+      saveStats()
     }
-    if (flag === "down" && level > 0) {
-      setLevel((prevLevel) => prevLevel - 1);
-    }
+
+    const changeLevelHandler = (flag) => {
+      if (flag === "up" && this.state.level <= 10) {
+        setState((state) => {
+          return { level: state.level + 1 };
+        })
+      }
+      if (flag === "down" && this.state.level > 0) {
+        setState((state) => {
+          return { level: state.level - 1 };
+        })
+      }
+    };
+
+    const updateLevelHandler = (newLevel) => {
+      this.setState({level:newLevel});
+    };
+
+
+    const popupOpenHandler = () => {
+      this.setState({popupped: true});
+    };
+
+    const popupCloseHandler = () => {
+      this.setState({popupped: false});
+    };
+
+    return (
+      <SafeAreaView style={styles.container}>
+        <PopUp popupped={this.state.popupped} closePopup={popupCloseHandler} />
+        <Modal>
+          <View style={styles.game}>
+            <View style={styles.header}>
+              <View style={styles.title}><Text style={styles.text}>Multiplikation Övning</Text></View>
+              <InfoButton title="i" onPress={popupOpenHandler} />
+            </View>
+            <Table level={this.state.level} popupped={this.state.popupped} />
+            <Calculator
+              level={this.state.level}
+              onUpdateLevel={updateLevelHandler}
+              onChangeLevel={(flag) => changeLevelHandler(flag)}
+              onSaveStats={()=>{this.saveStats()}}
+              onClearStats={()=>{this.clearStats()}}
+              onPassStats={passStatsHandler}
+              readStats={()=>{this.readStats()}}
+              savedStats={this.state.savedStats}
+            />
+          </View>
+        </Modal>
+        <StatusBar style="auto" />
+      </SafeAreaView>
+    );
   };
-  const updateLevelHandler = (newLevel) => {
-    setLevel(newLevel);
-  };
-
-
-  const popupOpenHandler = () => {
-    setPopupped(true);
-  };
-
-  const popupCloseHandler = () => {
-    console.log("pressed")
-    setPopupped(false);
-  };
-
-  return (
-    <SafeAreaView style={styles.container}>
-      <PopUp popupped={popupped} closePopup={popupCloseHandler} />
-      <Modal>
-      <View style={styles.game}>
-        <View style={styles.header}>
-          <View style={styles.title}><Text style={styles.text}>Multiplikation Övning</Text></View>
-          <InfoButton title="i" onPress={popupOpenHandler} />
-        </View>
-        <Table level={level} popupped={popupped}/>
-        <Calculator
-          level={level}
-          onUpdateLevel={updateLevelHandler}
-          onChangeLevel={(flag) => changeLevelHandler(flag)}
-          onSaveStats={saveStats}
-          onClearStats={clearStats}
-          onPassStats={passStatsHandler}
-          readStats={readStats}
-          savedStats={savedStats}
-        />
-        </View>
-      </Modal>
-      <StatusBar style="auto" />
-    </SafeAreaView>
-  );
-};
-
+}
 const styles = StyleSheet.create({
   container: {
     flex: 1,
@@ -161,7 +144,7 @@ const styles = StyleSheet.create({
     margin: "2%",
     marginLeft: "6%",
     elevation: 5,
-    
+
 
   },
   text: {
--- components/Calculator/Calculator.js
+++ components/Calculator/Calculator.js
@@ -1,276 +1,255 @@
-import React, { useState, useEffect, useRef } from "react";
+import React, { Component, createRef } from "react";
 import { StyleSheet, View, Button, AppState } from "react-native";
-import AsyncStorage from "@react-native-async-storage/async-storage";
 import NumberPad from "./NumberPad";
 import Screen from "./Screen";
 import ScoreTracker from "./ScoreTracker";
 import Timer from "./Timer";
 
-// const STORAGE_KEY = '@save_stats'
 
-const Calculator = (props) => {
-  console.log("Calculator")
-  // const [savedStats, setSavedStats] = useState(
-  //   '{"level":4,"product":"","products":["5 × 9","5 × 8","4 × 7","2 × 2","2 × 3","2 × 4","2 × 5","2 × 6","2 × 7","2 × 8","2 × 9","2 × 10","3 × 3","3 × 4","3 × 5","3 × 6","3 × 7","3 × 8","3 × 9","4 × 4","4 × 5","4 × 6","4 × 8","4 × 9","4 × 10","5 × 5","5 × 6","5 × 7","5 × 10","6 × 6","6 × 7","6 × 8","6 × 9","6 × 10","7 × 7","7 × 8","7 × 9","7 × 10","8 × 8","8 × 9","8 × 10","9 × 9","9 × 10","10 × 10"],"levelAttempts":true}'
-  // );
-  const [levelAttempts, setLevelAttempts] = useState(true);
-  const [started, setStarted] = useState(false);
-  const [product, setProduct] = useState("");
-  const [products, setProducts] = useState([]);
-  const [digits, setDigits] = useState("");
-  const [timerFlag, setTimerFlag] = useState("stop");
-  const [timerRate, setTimerRate] = useState(5);
 
-  const scoreTrackerRef = useRef();
-  const screenRef = useRef();
+class Calculator extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      levelAttempts: true,
+      started: false,
+      product: "",
+      products: ["5 × 9", "5 × 8", "4 × 7", "2 × 2", "2 × 3", "2 × 4", "2 × 5", "2 × 6", "2 × 7", "2 × 8", "2 × 9", "2 × 10", "3 × 3", "3 × 4", "3 × 5", "3 × 6", "3 × 7", "3 × 8", "3 × 9", "4 × 4", "4 × 5", "4 × 6", "4 × 8", "4 × 9", "4 × 10", "5 × 5", "5 × 6", "5 × 7", "5 × 10", "6 × 6", "6 × 7", "6 × 8", "6 × 9", "6 × 10", "7 × 7", "7 × 8", "7 × 9", "7 × 10", "8 × 8", "8 × 9", "8 × 10", "9 × 9", "9 × 10", "10 × 10"],
+      digits: "",
+      timerFlag: "stop",
+      timerRate: 5,
+    }
 
- 
+    this.scoreTrackerRef = createRef();
+    this.screenRef = createRef();
+    this.readStats = () => {
+      props.readStats()
+      setTimeout(() => {
+        let statsJson = { ...props.savedStats };
+        this.setState({ products: statsJson.products });
+        this.setState({ levelAttempts: statsJson.levelAttempts });
+      }, 250)
+    }
+  }
 
-  const readStats = () => {
-    props.readStats()
-    setTimeout(() => {
-    const splitStats = () => {
-      if (props.savedStats) {
-        let statsJson = JSON.parse(props.savedStats);
-        setProduct(statsJson.product);
-        setProducts(statsJson.products);
-        setLevelAttempts(statsJson.levelAttempts);
-        props.onUpdateLevel(statsJson.level);
-        console.log("read3")
+  render() {
+
+    /*
+    const _handleAppStateChange = (nextAppState) => {
+      if (nextAppState === "background" || nextAppState === "inactive") {
+        saveStats(savedStats);
       }
-    }
-    splitStats()}, 1000)
-  }
-
+    };
   
-
-
-  /*
-  const _handleAppStateChange = (nextAppState) => {
-    if (nextAppState === "background" || nextAppState === "inactive") {
-      saveStats(savedStats);
+    useEffect(() => {
+      AppState.addEventListener("change", _handleAppStateChange);
+      loadStats();
+      let statsJson = JSON.parse(savedStats);
+      setProduct(statsJson.product);
+      setProducts(statsJson.products);
+      setLevelAttempts(statsJson.levelAttempts);
+      props.onUpdateLevel(statsJson.level);
+  
+      return () => {
+        AppState.removeEventListener("change", _handleAppStateChange);
+      };
+    }, []);
+  
+  */
+    const passStatsHandler = () => {
+      let statsJson = {
+        products: this.state.products,
+        levelAttempts: this.state.levelAttempts,
+      };
+      props.onPassStats(statsJson)
     }
-  };
 
-  useEffect(() => {
-    AppState.addEventListener("change", _handleAppStateChange);
-    loadStats();
-    let statsJson = JSON.parse(savedStats);
-    setProduct(statsJson.product);
-    setProducts(statsJson.products);
-    setLevelAttempts(statsJson.levelAttempts);
-    props.onUpdateLevel(statsJson.level);
 
-    return () => {
-      AppState.removeEventListener("change", _handleAppStateChange);
-    };
-  }, []);
-
-*/
-  const passStatsHandler = () => {
-    let statsJson = {
-      level: props.level,
-      product: product,
-      products: products,
-      levelAttempts: levelAttempts,
-    };
-    let stats = JSON.stringify(statsJson);
-    console.log(stats)
-    props.onPassStats(stats)
-
-  }
-  // useEffect(() => {
-  //   let statsJson = {
-  //     level: props.level,
-  //     product: product,
-  //     products: products,
-  //     levelAttempts: levelAttempts,
-  //   };
-  //   let stats = JSON.stringify(statsJson);
-  //   setSavedStats(stats);
-  // }, [props.level, product, products]);
-
-  const generateProducts = () => {
-    const newProducts = [];
-    for (let n = 2; n < 11; n++) {
-      for (let m = 2; m < 11; m++) {
-        if (
-          !newProducts.includes(n + " × " + m) &&
-          !newProducts.includes(m + " × " + n)
-        ) {
-          newProducts.push(n + " × " + m);
+    const _generateProducts = () => {
+      const newProducts = [];
+      for (let n = 2; n < 11; n++) {
+        for (let m = 2; m < 11; m++) {
+          if (
+            !newProducts.includes(n + " × " + m) &&
+            !newProducts.includes(m + " × " + n)
+          ) {
+            newProducts.push(n + " × " + m);
+          }
         }
       }
-    }
-    return newProducts;
-  };
+      return newProducts;
+    };
 
-  const generateProduct = () => {
-    let productsArray = [...products];
-    if (productsArray.length < 1) {
-      productsArray = generateProducts();
-      props.onChangeLevel("up");
-    }
-    if (productsArray.length >= 50) {
-      productsArray = generateProducts();
-
-      props.onChangeLevel("down");
-    }
-    const randomIndex = Math.floor(Math.random() * productsArray.length);
-    const newProduct = productsArray.splice(randomIndex, 1);
-    setProduct(newProduct[0]);
-    setProducts([...productsArray]);
-  };
-
-  const startHandler = () => {
-    if (!started) {
-      setDigits("");
-      setStarted(true);
-      setTimerFlag("start");
-      generateProduct();
-      screenRef.current.changeTextColor("black");
-    }
-  };
-
-  const stopHandler = () => {
-    if (started) {
-      setDigits("");
-      setProducts([product, ...products]);
-      screenRef.current.changeTextColor("transparent");
-      setStarted(false);
-      setTimerFlag("stop");
-    }
-  };
-
-  const checkProductHandler = (answer) => {
-    const numbers = product.split(" × ");
-    const result = parseInt(numbers[0]) * parseInt(numbers[1]);
-    if (parseInt(answer) === result) {
-      setDigits("");
-      screenRef.current.changeInputColor("#79e36d");
-      setTimeout(() => {
-        screenRef.current.changeInputColor("#ccd4cb");
-      }, 250);
-      generateProduct();
-      if (timerFlag === "reset-on") {
-        setTimerFlag("reset-off");
-      } else {
-        setTimerFlag("reset-on");
+    const generateProduct = () => {
+      let productsArray = [...this.state.products];
+      if (productsArray.length < 1) {
+        productsArray = _generateProducts();
+        props.onChangeLevel("up");
       }
-    } else {
-      screenRef.current.changeInputColor("#f75252");
-      setProducts([product, ...products]);
-      if (products.length >= 50) {
+      if (productsArray.length >= 50) {
+        productsArray = _generateProducts();
+
+        props.onChangeLevel("down");
+      }
+      const randomIndex = Math.floor(Math.random() * productsArray.length);
+      const newProduct = productsArray.splice(randomIndex, 1);
+      this.setState({ product: newProduct[0], products: [...productsArray] });
+    };
+
+    const startHandler = () => {
+      if (!this.state.started) {
+        this.setState({
+          digits: "",
+          started: true,
+          timerFlag: "start",
+        })
         generateProduct();
+        this.screenRef.current.changeTextColor("black");
       }
-      if (timerFlag === "reset-on") {
-        setTimerFlag("reset-off");
-      } else {
-        setTimerFlag("reset-on");
-      }
-      setTimeout(() => {
-        screenRef.current.changeInputColor("#ccd4cb");
+    };
+
+    const stopHandler = () => {
+      if (started) {
         setDigits("");
-      }, 250);
-    }
-  };
+        setProducts([product, ...products]);
+        this.screenRef.current.changeTextColor("transparent");
+        setStarted(false);
+        setTimerFlag("stop");
+      }
+    };
 
-  const deleteHandler = () => {
-    if (digits.length > 0 && started) {
-      setDigits((prevDigits) => prevDigits.slice(0, -1));
-    } else {
-      setDigits("");
-    }
-  };
+    const checkProductHandler = (answer) => {
+      const numbers = product.split(" × ");
+      const result = parseInt(numbers[0]) * parseInt(numbers[1]);
+      if (parseInt(answer) === result) {
+        setDigits("");
+        this.screenRef.current.changeInputColor("#79e36d");
+        setTimeout(() => {
+          this.screenRef.current.changeInputColor("#ccd4cb");
+        }, 250);
+        generateProduct();
+        if (timerFlag === "reset-on") {
+          setTimerFlag("reset-off");
+        } else {
+          setTimerFlag("reset-on");
+        }
+      } else {
+        this.screenRef.current.changeInputColor("#f75252");
+        setProducts([product, ...products]);
+        if (products.length >= 50) {
+          generateProduct();
+        }
+        if (timerFlag === "reset-on") {
+          setTimerFlag("reset-off");
+        } else {
+          setTimerFlag("reset-on");
+        }
+        setTimeout(() => {
+          this.screenRef.current.changeInputColor("#ccd4cb");
+          setDigits("");
+        }, 250);
+      }
+    };
 
-  const enterHandler = () => {
-    if (digits.length > 0 && started) {
-      checkProductHandler(digits);
-      setStarted(true);
-    }
-    if (digits.length === 3 && !started && digits.slice(0, 2) === "13" && digits[2] !== "0") {
-      props.onUpdateLevel(digits[2]);
-      setProducts(generateProducts());
-      setDigits("");
-    }
-    if (digits.length === 3 && !started && digits.slice(0, 2) === "77" && digits[2] !== "0") {
-      setTimerRate(digits[2]);
-      setDigits("");
-    }
-    if (!started) {
-      setDigits("");
-    }
-  };
+    const deleteHandler = () => {
+      if (digits.length > 0 && started) {
+        setDigits((prevDigits) => prevDigits.slice(0, -1));
+      } else {
+        setDigits("");
+      }
+    };
 
-  const enteredDigitsHandler = (newDigit) => {
-    if (digits.length < 3) {
-      setDigits((prevDigits) => prevDigits + newDigit);
-    }
-  };
+    const enterHandler = () => {
+      if (digits.length > 0 && started) {
+        checkProductHandler(digits);
+        setStarted(true);
+      }
+      if (digits.length === 3 && !started && digits.slice(0, 2) === "13" && digits[2] !== "0") {
+        props.onUpdateLevel(digits[2]);
+        setProducts(generateProducts());
+        setDigits("");
+      }
+      if (digits.length === 3 && !started && digits.slice(0, 2) === "77" && digits[2] !== "0") {
+        setTimerRate(digits[2]);
+        setDigits("");
+      }
+      if (!started) {
+        setDigits("");
+      }
+    };
 
-  useEffect(() => {
-    let barFillHeight = Math.round((products.length / 50) * 85) + "%";
-    let warningLevel = products.length;
-    if (warningLevel === 44) {
-      scoreTrackerRef.current.changeTrackerColor("#4da6ff");
-    }
-    if (warningLevel === 47) {
-      scoreTrackerRef.current.changeTrackerColor("#cc99ff");
-    }
-    if (warningLevel === 48) {
-      scoreTrackerRef.current.changeTrackerColor("#ff9980");
-    }
-    if (warningLevel === 49) {
-      scoreTrackerRef.current.changeTrackerColor("#ff3333");
-    }
+    const enteredDigitsHandler = (newDigit) => {
+      if (digits.length < 3) {
+        this.setState((state) => {
+          return { digits: state.digits + newDigit }
+        });
+      }
+    };
 
-    scoreTrackerRef.current.changeHeight(barFillHeight.toString());
-  }, [products, product]);
+    const updateTracker = () => {
+      let barFillHeight = Math.round((this.state.products.length / 50) * 85) + "%";
+      let warningLevel = this.state.products.length;
+      if (warningLevel === 44) {
+        this.scoreTrackerRef.current.changeTrackerColor("#4da6ff");
+      }
+      if (warningLevel === 47) {
+        this.scoreTrackerRef.current.changeTrackerColor("#cc99ff");
+      }
+      if (warningLevel === 48) {
+        this.scoreTrackerRef.current.changeTrackerColor("#ff9980");
+      }
+      if (warningLevel === 49) {
+        this.scoreTrackerRef.current.changeTrackerColor("#ff3333");
+      }
 
-  const outOfTimeHandler = () => {
-    if (levelAttempts) {
-      setLevelAttempts(false);
-    } else {
-      setLevelAttempts(true);
-      props.onChangeLevel("down");
-    }
-  };
+      this.scoreTrackerRef.current.changeHeight(barFillHeight.toString());
+    };
 
-  return (
-    <View className="calculator" style={styles.calculator}>
-      <Screen ref={screenRef} product={product} digits={digits} key="screen" />
-      <View style={styles.calculatorBody}>
-        <ScoreTracker
-          style={styles.scoreTracker}
-          ref={scoreTrackerRef}
-          level={props.level}
-        />
-        <Timer
-          style={styles.timer}
-          timerFlag={timerFlag}
-          level={props.level}
-          onOutOfTime={outOfTimeHandler}
-          timerRate={timerRate}
-        />
-        <NumberPad
-          style={styles.numberPad}
-          digits={digits}
-          onEnteredDigit={enteredDigitsHandler}
-          onDelete={deleteHandler}
-          onEnter={enterHandler}
-          onStart={!started ? startHandler : stopHandler}
-          started={started}
-          onOutOfTime={outOfTimeHandler}
-        />
+    const outOfTimeHandler = () => {
+      if (this.state.levelAttempts) {
+        this.setState({ levelAttempts: false });
+      } else {
+        this.setState({ levelAttempts: true });
+        props.onChangeLevel("down");
+      }
+    };
+
+    return (
+      <View className="calculator" style={styles.calculator}>
+        <Screen ref={this.screenRef} product={this.product} digits={this.digits} key="screen" />
+        <View style={styles.calculatorBody}>
+          <ScoreTracker
+            style={styles.scoreTracker}
+            ref={this.scoreTrackerRef}
+            level={props.level}
+          />
+          <Timer
+            style={styles.timer}
+            timerFlag={this.state.timerFlag}
+            level={props.level}
+            onOutOfTime={outOfTimeHandler}
+            timerRate={this.state.timerRate}
+          />
+          <NumberPad
+            style={styles.numberPad}
+            digits={this.state.digits}
+            onEnteredDigit={enteredDigitsHandler}
+            onDelete={deleteHandler}
+            onEnter={enterHandler}
+            onStart={!this.state.started ? startHandler : stopHandler}
+            started={this.state.started}
+            onOutOfTime={outOfTimeHandler}
+          />
+        </View>
+        <View style={{ flexDirection: "row" }}>
+          <Button title="save" onPress={passStatsHandler} />
+          <Button title="clear" onPress={props.onclearStats} />
+          <Button title="read" onPress={this.readStats} />
+        </View>
       </View>
-      <View style={{flexDirection: "row"}}>
-      <Button title="save" onPress={passStatsHandler} />
-      <Button title="clear" onPress={props.onclearStats} />
-      <Button title="read" onPress={readStats} />
-      </View>
-    </View>
-  );
-};
+    );
+  };
+}
 
 const styles = StyleSheet.create({
   calculator: {
